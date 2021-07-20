e77b50fcbaa17116c8aa4c409f9847fed3fe7e83
Bryan Rodriguez <bsteven1993@gmail.com>
2021-07-19 20:23:18 -0500

    se agrega docs

---
+++ docs/precache-manifest.f80f5ce78f0c607911e3bb071a9361a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a462f871a3a2a0c99b49788286e4888a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3872f87eec62d3ec88d",
+    "url": "/static/css/main.42c17b65.chunk.css"
+  },
+  {
+    "revision": "eb0fa122e48c7a265623",
+    "url": "/static/js/2.e396b283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e396b283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3872f87eec62d3ec88d",
+    "url": "/static/js/main.feab7c84.chunk.js"
+  },
+  {
+    "revision": "3c6c944527f6d17fde20",
+    "url": "/static/js/runtime-main.62f8418f.js"
+  }
+]);
